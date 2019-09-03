1692be65e45392b6ac1ee80e1e68ea58a9e3bc4f
esim7 <makishev@yahoo.com>
2019-09-03 14:44:55 +0600

    Доработал задание

--- home-work-7/Program.cs
+++ home-work-7/Program.cs
@@ -13,30 +13,32 @@ namespace home_work_7
             //Олег Сергеевич, честно говоря не понял задания, там сказано что в методе нужно обрабатывать исключения
             //если в делитель попадает 0 то отлавливаем с этим все понятно, вторая часть задания отловить если в методе не заданы агрументы, 
             //но как... ведь если их не указать то ВС итак не скомпилирует.
-            //int Division(int leftNumber, int rightNumber)
-            //{
-            //    try
-            //    {
-            //        return leftNumber / rightNumber;
-            //    }
-            //    catch (DivideByZeroException)
-            //    {
-            //        Console.WriteLine("Ошибка, делитель не может быть 0");
-            //    }
-            //    return 0;
-            //}
-            //Console.WriteLine("Введите число №1");
-            //int firstNumber = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Введите число №2");
-            //int secondNumber = int.Parse(Console.ReadLine());
-            //Console.WriteLine(Division(firstNumber, secondNumber));
-            //Console.ReadKey();
+            
+            //задание 1
+            int Division(int leftNumber, int rightNumber)
+            {
+                try
+                {
+                    return leftNumber / rightNumber;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Ошибка, делитель не может быть 0");
+                }
+                return 0;
+            }
+            Console.WriteLine("Введите число №1");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число №2");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Division(firstNumber, secondNumber));
+            Console.ReadKey();
 
             //задание 2
             Console.WriteLine("Введите размер массива");
             int size = int.Parse(Console.ReadLine());
             int[] mas = new int[size];
-            for(int i = 0; i < mas.Length; i++)
+            for (int i = 0; i < mas.Length; i++)
             {
                 Console.WriteLine("Введите значение элемента " + i);
                 mas[i] = int.Parse(Console.ReadLine());
@@ -50,7 +52,7 @@ namespace home_work_7
             }
             catch (IndexOutOfRangeException)
             {
-                Console.WriteLine("\nВыход за границы массива!");
+                Console.WriteLine("\nпопытка обратиться к " + (mas.Length) + " элементу\nВыход за границы массива!");
             }
             finally
             {
